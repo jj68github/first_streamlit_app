ea4e443bcb9d6092c9b0f6ee26ef236240c2616b
jj68github <153678107+jj68github@users.noreply.github.com>
2023-12-15 10:27:56 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLerror
 streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -9,7 +13,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -17,24 +22,22 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'] )
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 # new section to display Fruityvice Fruit Advice
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
+streamlit.stop()
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -44,3 +47,4 @@ streamlit.dataframe(my_data_rows)
 # allow user to add fruit list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfuirt')
 streamlit.write('Thank you for adding ',add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
